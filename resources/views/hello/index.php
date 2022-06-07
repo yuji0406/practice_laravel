7c43b07774afa95f63ce663988b6e9187961afdf
y.kawaguchi <>
2022-06-07 22:38:04 +0900

    もう一回コミット

---
+++ resources/views/hello/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Hello/Index</title>
+        <style>
+            body { font-size: 16pt; color: #999;}
+            h1 { font-size: 100pt; text-align: right; color: #f6f6f6;
+                margin: -50px 0 -100px 0px; }
+        </style>
+    </head>
+    <body>
+        <h1>Index</h1>
+        <p><?php echo $msg; ?></p>
+        <p>ID: <?php echo $id; ?></p>
+        <p>This is sample page with php-template</p>
+    </body>
+</html>
