754a740d2d508b9a9b7b61ec4032f9c106146059
yuji0406 <uver_35@yahoo.co.jp>
2022-06-15 22:18:48 +0900

    　ServiceProviderの登録

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Original Service Providers...
+         */
+        App\Providers\HelloServiceProvider::class
     ],
 
     /*
