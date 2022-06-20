23c65f4d53e1fce7fd67a1acc96bdbde245a1961
yuji0406 <uver_35@yahoo.co.jp>
2022-06-20 22:01:00 +0900

    コントローラ処理後のミドルウェア

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -15,12 +15,14 @@ class HelloMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $data = [
-            ['name' => 'taro', 'mail' => 'taro@sample.com'],
-            ['name' => 'hanako', 'mail' => 'hanako@sample.com'],
-            ['name' => 'jiro', 'mail' => 'jiro@sample.com'],
-        ];
-        $request->merge(['data' => $data]);
-        return $next($request);
+        $response = $next($request);
+        $content = $response->content();
+
+        $pattern = '/<middleware>(.*)<\/middleware>/i';
+        $replace = '<a href="http://$1">$1</a>';
+        $content = preg_replace($pattern, $replace, $content);
+        $response->setContent($content);
+
+        return $response;
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,19 +9,8 @@
 
 @section('content')
     <p>ここが本文のコンテンツです。</p>
-    <table>
-        @foreach ($data as $item)
-        <tr>
-            <th>
-                {{ $item['name'] }}
-            </th>
-            <td>
-                {{ $item['mail'] }}
-            </td>
-        </tr>
-        @endforeach
-    </table>
-    <div id="result"></div>
+    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
+    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
 @endsection
 
 @section('footer')
