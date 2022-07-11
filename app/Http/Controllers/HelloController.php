087f26b038cdde81b6070756fb8acf65e39f4d58
yuji0406 <uver_35@yahoo.co.jp>
2022-07-11 21:11:48 +0900

    バリデータを作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
@@ -15,6 +16,18 @@ class HelloController extends Controller
 
     public function post(HelloRequest $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'requied',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
