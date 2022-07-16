1ef4c7500996560b1816a8995ddc6b3910c8d451
yuji0406 <uver_35@yahoo.co.jp>
2022-07-16 21:57:50 +0900

    グローバルスコープ作成

--- app/Person.php
+++ app/Person.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('age', function(Builder $builder) {
+            $builder->where('age', '>', 20);
+        });
+    }
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
