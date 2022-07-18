6e56f0a3b5e09b708ead8d5bb09177836e8c0a6c
yuji0406 <uver_35@yahoo.co.jp>
2022-07-18 19:57:32 +0900

    リソースの作成

---
+++ database/seeds/RestdataTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Restdata;
+use Illuminate\Database\Seeder;
+
+class RestdataTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'message' => 'Google Japan',
+            'url' => 'https://www.google.co.jp'
+        ];
+
+        $restdata = new Restdata;
+        $restdata->fill($param)->save();
+
+        $param = [
+            'message' => 'Yahoo Japan',
+            'url' => 'https://www.yahoo.co.jp'
+        ];
+
+        $restdata = new Restdata;
+        $restdata->fill($param)->save();
+
+        $param = [
+            'message' => 'MSN Japan',
+            'url' => 'https://www.msn.com/ja-jp'
+        ];
+
+        $restdata = new Restdata;
+        $restdata->fill($param)->save();
+    }
+}
